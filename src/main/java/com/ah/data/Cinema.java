34ada218be89e7e287bc68b5d0b5eb0de0d2848b
Anthony-Harrison <AnthonyHarrison.job@gmail.com>
2021-10-12 19:37:59 +0100

    Feature2-Cinema CRUD complete

--- src/main/java/com/ah/data/Cinema.java
+++ src/main/java/com/ah/data/Cinema.java
@@ -1,5 +1,76 @@
 package com.ah.data;
 
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cinema {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String branch;
+	private int noOfScreens;
+
+	public Cinema() {
+		super();
+	}
+
+	public Cinema(Integer id, String branch, int noOfScreens) {
+		super();
+		this.id = id;
+		this.branch = branch;
+		this.noOfScreens = noOfScreens;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public int getNoOfScreens() {
+		return noOfScreens;
+	}
+
+	public void setNoOfScreens(int noOfScreens) {
+		this.noOfScreens = noOfScreens;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(branch, id, noOfScreens);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cinema other = (Cinema) obj;
+		return Objects.equals(branch, other.branch) && Objects.equals(id, other.id) && noOfScreens == other.noOfScreens;
+	}
+
+	@Override
+	public String toString() {
+		return "Cinema [id=" + id + ", branch=" + branch + ", noOfScreens=" + noOfScreens + "]";
+	}
 
 }
--- src/main/java/com/ah/service/CinemaService.java
+++ src/main/java/com/ah/service/CinemaService.java
@@ -1,5 +1,47 @@
 package com.ah.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.ah.data.Cinema;
+import com.ah.repo.CinemaRepo;
+
+@Service
 public class CinemaService {
 
+	private CinemaRepo repo;
+
+	public CinemaService(CinemaRepo repo) {
+		super();
+		this.repo = repo;
+	}
+
+	public Cinema getCinemaById(Integer id) {
+		return this.repo.findById(id).get();
+	}
+
+	public List<Cinema> getAllCinemas() {
+		return this.repo.findAll();
+	}
+
+	public Cinema createCinema(Cinema cinema) {
+		return this.repo.save(cinema);
+	}
+
+	public Cinema updateCinema(Cinema cinema, Integer id) {
+		Optional<Cinema> optionalCinema = this.repo.findById(id);
+		Cinema toUpdate = optionalCinema.get();
+		toUpdate.setBranch(cinema.getBranch());
+		toUpdate.setNoOfScreens(cinema.getNoOfScreens());
+
+		return this.repo.save(toUpdate);
+	}
+
+	public boolean deleteCinema(Integer id) {
+		this.repo.deleteById(id);
+		boolean exists = this.repo.existsById(id);
+		return !exists;
+	}
 }
